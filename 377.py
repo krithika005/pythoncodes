878ad1bb10034bee6e3627b5eb92c247c85abd3f
krithika005 <mrkrithika30@gmail.com>
2025-01-03 22:14:07 +0530

    Add files via upload

---
+++ 377.py
@@ -0,0 +1,332 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a = 25
+print(a)
+type(a)
+
+
+# In[1]:
+
+
+b = "nicky"
+print(b)
+type(b)
+
+
+# In[3]:
+
+
+c = 'true'
+print(c)
+type(c)
+
+
+# In[5]:
+
+
+d = False
+type(d)
+
+
+# In[6]:
+
+
+e = 32.9
+print(e)
+type(e)
+
+
+# In[2]:
+
+
+### addition +
+### subtraction -
+### multiplication *
+### division /
+### remainder %
+### power **
+### // floor division
+
+
+# In[3]:
+
+
+a = 109
+b = 3
+
+
+# In[4]:
+
+
+a + b
+
+
+# In[5]:
+
+
+c = a + b
+c
+
+
+# In[6]:
+
+
+type(c)
+
+
+# In[7]:
+
+
+d = a - b
+d
+
+
+# In[9]:
+
+
+type(d)
+
+
+# In[10]:
+
+
+e = a * b
+e
+
+
+# In[11]:
+
+
+type(e)
+
+
+# In[12]:
+
+
+f = a / b
+f
+
+
+# In[13]:
+
+
+type(f)
+
+
+# In[14]:
+
+
+g = a ** b
+g
+
+
+# In[15]:
+
+
+type(g)
+
+
+# In[16]:
+
+
+h = a % b
+h
+
+
+# In[17]:
+
+
+type(h)
+
+
+# In[18]:
+
+
+i = a // b
+i
+
+
+# In[19]:
+
+
+type(i)
+
+
+# In[22]:
+
+
+### > < >= <= == != 
+
+
+# In[23]:
+
+
+5>6
+
+
+# In[24]:
+
+
+3<8
+
+
+# In[25]:
+
+
+4>=7
+
+
+# In[26]:
+
+
+5<=6
+
+
+# In[27]:
+
+
+7==9
+
+
+# In[28]:
+
+
+8 != 9
+
+
+# In[29]:
+
+
+a=4
+b=7
+
+
+# In[31]:
+
+
+a>b
+
+
+# In[32]:
+
+
+a<b
+
+
+# In[33]:
+
+
+a>=b
+
+
+# In[34]:
+
+
+a<=b
+
+
+# In[35]:
+
+
+a==b
+
+
+# In[36]:
+
+
+a!=b
+
+
+# In[39]:
+
+
+# logical operators
+# and 
+# or
+# not
+
+
+# In[40]:
+
+
+a=5
+b=9
+
+
+# In[41]:
+
+
+(a > 80)
+
+
+# In[42]:
+
+
+(a < b) or (a > 20)
+
+
+# In[43]:
+
+
+(a > b) and (b > 50)
+
+
+# In[47]:
+
+
+not (a > 5)
+
+
+# In[48]:
+
+
+# all arithmetic operators
+# all comparision operators
+# and or
+
+
+# In[49]:
+
+
+d
+
+
+# In[50]:
+
+
+d > 10
+
+
+# In[51]:
+
+
+d < 50
+
+
+# In[52]:
+
+
+d > 10 and c < 40
+
+
+# In[53]:
+
+
+a > b or c < d
+
+
+# In[54]:
+
+
+not c > a
+
+
+# In[ ]:
+
+
+
+
